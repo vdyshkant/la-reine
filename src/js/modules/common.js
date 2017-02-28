8f76475adf4556b760f1ebb6912e52f537e9bb62
vdyshkant <yong.knox@yandex.ru>
2017-02-28 19:07:39 +0300

    index::brills -- made falling items (product-items-top module)

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -913,3 +913,36 @@ function animateRedLine() {
 
 
 /* == eof $ MAIN == */
+
+
+
+// parallax-brills
+(function() {
+    var isIE = navigator.userAgent.indexOf("MSIE ") > 0 || navigator.userAgent.indexOf("Trident") > 0 || navigator.userAgent.indexOf("Edge") > 0,
+        wScroll = $(window).scrollTop();
+
+    // parallax effect function
+    function parallax(prlxBg, prlxContainer, factor) {
+        if (isIE) {
+            $(prlxBg).css({
+                'transform': 'translateY(0px)'
+            });
+            return;
+        }
+        if ((wScroll + $(window).height()) >= $(prlxBg).offset().top) {
+            console.log("true!");
+            $(prlxBg).css({
+                'transform': 'translateY(' + (($(prlxContainer).offset().top - wScroll) / $(window).height() * 100) * factor + '%)'
+            });
+        }
+    }
+
+    $(window).scroll(function() {
+        wScroll = $(this).scrollTop();
+
+        if ($('.parallax-index').length > 0) {
+            parallax('.brills-parallax-box', '.parallax-index', -3.6);
+        }
+    });
+})();
+// EOFparallax-brills
